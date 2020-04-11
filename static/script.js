8c14512a625478c66af902dfd1974b56933d8b64
Steven <stevenandryy@gmail.com>
2020-04-11 14:33:44 +0700

    tidying code and added linewidth counter function

--- static/script.js
+++ static/script.js
@@ -1,5 +1,8 @@
 
 // window.onload = function() { 
+$(window).on("load", function () {
+    $(".loader-wrapper").fadeOut("slow");
+});
 var canvas = document.getElementById("paint");
 var ctx = canvas.getContext("2d");
 var imgs = document.getElementById("imagetest");
@@ -11,153 +14,158 @@ var height = canvas.height;
 var curX, curY, prevX, prevY;
 var hold = false;
 ctx.lineWidth = 3;
+var defaultlinewidth = ctx.lineWidth;
 var fill_value = true;
 var stroke_value = false;
 // var canvas_data = {"pencil": [], "line": [], "rectangle": [], "circle": [], "eraser": []}
-var canvas_data = {"pencil": [], "rectangle": []}
-var image = new Image();
-    console.log(image);
-    image.onload = function(e) {
-        ctx.canvas.width = image.width;
-        ctx.canvas.height = image.height;
-        c.width = image.width;
-        c.height = image.height;
-        ctx.drawImage(image, 0, 0);
-        console.log(labels);
-        for (i = 0; i < labels.length; i++){
-            drawLabels(labels[i].id, labels[i].xMin, labels[i].xMax, labels[i].yMin, labels[i].yMax);
-        }
-    };
-    image.style.display="block";
-    image.src = "image/{{ image }}";
-    var clicked = false;
-      var fPoint = {};
-      c.onclick = function(e) {
-        console.log(clicked);
-        if (!clicked) {
-            var x = (image.width / c.scrollWidth) * e.offsetX;
-            var y = (image.height / c.scrollHeight) * e.offsetY;
-            console.log(e);
-            ctx.strokeStyle = "pink";
-            ctx.fillStyle = "pink";
-            ctx.beginPath();
-            ctx.arc(x, y, 3, 0, 2*Math.PI, false);
-            ctx.fill();
-            fPoint = {
-              x: x,
-              y: y
-            };
-        } else {
-            var x = (image.width / c.scrollWidth) * e.offsetX;
-            var y = (image.height / c.scrollHeight) * e.offsetY;
-            var xMin;
-            var xMax;
-            var yMin;
-            var yMin;
-            if (x > fPoint.x) {
-                xMax = x;
-                xMin = fPoint.x;
-            } else {
-                xMax = fPoint.x;
-                xMin = x;
-            }
-            if (y > fPoint.y) {
-              yMax = y;
-              yMin = fPoint.y;
-            } else {
-              yMax = fPoint.y;
-              yMin = y;
-            }
-            fPoint = {};
-            window.location.replace("/add/" + (labels.length + 1) +
-            "?xMin=" + xMin +
-            "&xMax=" + xMax +
-            "&yMin=" + yMin +
-            "&yMax=" + yMax);
-        }
-        clicked = !clicked;
-      };
+var canvas_data = { "pencil": [], "rectangle": [] }
+// var image = new Image();
+//     console.log(image);
+//     image.onload = function(e) {
+//         ctx.canvas.width = image.width;
+//         ctx.canvas.height = image.height;
+//         c.width = image.width;
+//         c.height = image.height;
+//         ctx.drawImage(image, 0, 0);
+//         console.log(labels);
+//         for (i = 0; i < labels.length; i++){
+//             drawLabels(labels[i].id, labels[i].xMin, labels[i].xMax, labels[i].yMin, labels[i].yMax);
+//         }
+//     };
+//     image.style.display="block";
+//     image.src = "image/{{ image }}";
+//     var clicked = false;
+//       var fPoint = {};
+//       c.onclick = function(e) {
+//         console.log(clicked);
+//         if (!clicked) {
+//             var x = (image.width / c.scrollWidth) * e.offsetX;
+//             var y = (image.height / c.scrollHeight) * e.offsetY;
+//             console.log(e);
+//             ctx.strokeStyle = "pink";
+//             ctx.fillStyle = "pink";
+//             ctx.beginPath();
+//             ctx.arc(x, y, 3, 0, 2*Math.PI, false);
+//             ctx.fill();
+//             fPoint = {
+//               x: x,
+//               y: y
+//             };
+//         } else {
+//             var x = (image.width / c.scrollWidth) * e.offsetX;
+//             var y = (image.height / c.scrollHeight) * e.offsetY;
+//             var xMin;
+//             var xMax;
+//             var yMin;
+//             var yMin;
+//             if (x > fPoint.x) {
+//                 xMax = x;
+//                 xMin = fPoint.x;
+//             } else {
+//                 xMax = fPoint.x;
+//                 xMin = x;
+//             }
+//             if (y > fPoint.y) {
+//               yMax = y;
+//               yMin = fPoint.y;
+//             } else {
+//               yMax = fPoint.y;
+//               yMin = y;
+//             }
+//             fPoint = {};
+//             window.location.replace("/add/" + (labels.length + 1) +
+//             "?xMin=" + xMin +
+//             "&xMax=" + xMax +
+//             "&yMin=" + yMin +
+//             "&yMax=" + yMax);
+//         }
+//         clicked = !clicked;
+//       };
 
-
-function color(color_value){
+function color(color_value) {
     ctx.strokeStyle = color_value;
     ctx.fillStyle = color_value;
-}    
-        
-function add_pixel(){
+}
+
+function add_pixel() {
     ctx.lineWidth += 1;
+    defaultlinewidth += 1;
+    document.getElementById("currentlw").innerHTML = defaultlinewidth;
 }
-        
-function reduce_pixel(){
-    if (ctx.lineWidth == 1){
+
+function reduce_pixel() {
+    if (ctx.lineWidth == 1) {
         ctx.lineWidth = 1;
+        defaultlinewidth = 1;
     }
-    else{
+    else {
         ctx.lineWidth -= 1;
+        defaultlinewidth -= 1;
+        document.getElementById("currentlw").innerHTML = defaultlinewidth;
     }
 }
-        
-function reset(){
+
+function reset() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // generatecanvas();
-    ctx.drawImage(imgs, 0, 0, canvas.width, canvas.height); //draw the image upon reset, which is called on load
+    ctx.drawImage(imgs, 0, 0, width, height); //draw the image upon reset, which is called on load
     // canvas_data = { "pencil": [], "line": [], "rectangle": [], "circle": [], "eraser": [] }
-    canvas_data = {"pencil": [],"rectangle": []}
+    canvas_data = { "pencil": [], "rectangle": [] }
     rectangle();
 }
-          
-function rectangle(){
+
+function rectangle() {
     fill_value = false;
-    stroke_value = true; 
+    stroke_value = true;
     color('#EE0000');
-    canvas.onmousedown = function (e){
-        img = ctx.getImageData(0, 0, width, height);
+    canvas.onmousedown = function (e) {
+        img = ctx.getImageData(0, 0, width, height); //supposed to be width & height var, but changed so that it takes the width and height realtime
         prevX = e.clientX - canvas.offsetLeft;
         prevY = e.clientY - canvas.offsetTop;
         hold = true;
     };
-            
-    canvas.onmousemove = function (e){
-        if (hold){
+
+    canvas.onmousemove = function (e) {
+        if (hold) {
             ctx.putImageData(img, 0, 0);
             curX = e.clientX - canvas.offsetLeft - prevX;
             curY = e.clientY - canvas.offsetTop - prevY;
             ctx.strokeRect(prevX, prevY, curX, curY);
-            if (fill_value){
+            if (fill_value) {
                 ctx.fillRect(prevX, prevY, curX, curY);
             }
             canvas_data.rectangle.push({ "starx": prevX, "stary": prevY, "width": curX, "height": curY, "thick": ctx.lineWidth, "stroke": stroke_value, "stroke_color": ctx.strokeStyle, "fill": fill_value, "fill_color": ctx.fillStyle });
-            
+
         }
     };
-            
-    canvas.onmouseup = function(e){
+
+    canvas.onmouseup = function (e) {
         hold = false;
     };
-            
-    canvas.onmouseout = function(e){
+
+    canvas.onmouseout = function (e) {
         hold = false;
     };
 }
 
-function fill(){
+function fill() {
     fill_value = true;
     stroke_value = false;
 }
-        
-function outline(){
+
+function outline() {
     fill_value = false;
     stroke_value = true;
 }
-        
-function pencil(){
-    
-    canvas.onmousedown = function(e){
+
+function pencil() {
+
+    canvas.onmousedown = function (e) {
         // getPosition(e); 
         curX = e.clientX - canvas.offsetLeft;
         curY = e.clientY - canvas.offsetTop;
         drawCoordinates(curX, curY);
-            
+
         prevX = curX;
         prevY = curY;
         // // ctx.beginPath();
@@ -165,15 +173,15 @@ function pencil(){
     };
     // var pointSize = 3;
 
-    function drawCoordinates(x,y){	
-    	ctx.fillStyle = "#ff2626"; // Red color
+    function drawCoordinates(x, y) {
+        ctx.fillStyle = "#ff2626"; // Red color
         ctx.beginPath();
         ctx.arc(x, y, ctx.lineWidth, 0, Math.PI * 2, true);
         ctx.fill();
         canvas_data.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
     }
 
-     // function draw(){
+    // function draw(){
     //     ctx.lineTo(curX, curY);
     //     ctx.stroke();
     //     canvas_data.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
@@ -187,34 +195,34 @@ function pencil(){
     // }
 }
 
-function save(){
+function save() {
     var filename = document.getElementById("fname").value;
     var data = JSON.stringify(canvas_data);
     var image = canvas.toDataURL();
 
-    if(filename==""){
+    if (filename == "") {
         alert("Image name must be filled!");
 
-    }else{
-    
-    $.post("/profile", {save_fname: filename, save_cdata: data, save_image: image});
-    alert(filename + " saved");
+    } else {
+
+        $.post("/profile", { save_fname: filename, save_cdata: data, save_image: image });
+        alert(filename + " saved");
     }
     // else {
     //     $.post("/"+imagename.value,{imagename:imagename.value, string:JSON.stringify(data)});
-	// 	alert("saved");
-	// }
-} 
+    // 	alert("saved");
+    // }
+}
 
 // function generatecanvas(){
 //     var resizewidth = document.getElementById("canvas-width");
 //     var resizeheight = document.getElementById("canvas-height");
-    
+
 //     $.post("/resize", {canvaswidth : resizewidth, canvasheight: resizeheight});
 //     alert("Canvas set to " + resizewidth + "width and " + resizeheight + "height");
-    
+
 // }
 
-function alerttest(){
+function alerttest() {
     alert("success!");
 }
