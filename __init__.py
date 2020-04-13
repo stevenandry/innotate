6b12df50ae688555f91a1573866eadebdce97be3
Steven <stevenandryy@gmail.com>
2020-04-13 14:23:41 +0700

    supressed sqlalchemy_track_modifications warning

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
 	app = Flask(__name__)
-
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
