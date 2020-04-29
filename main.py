9dc7c5b367ea7b30fbcdf59d3986bdde3c125948
Steven <stevenandryy@gmail.com>
2020-04-29 12:21:20 +0700

    Added mysql integration and push

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from flask import Flask, redirect, url_for, request, send_file
 from . import db
 from flask_login import login_required, current_user
+import MySQLdb
+# from flask_mysqldb import MySQL > ini gabisa pake connection string langsung, harus lewat app config maka nya
+# gajadi pake
+# import MySQLdb > gajadi dibuat disini, krn ternyata bs ambil function dr file lain.
+# and as suggested in on of sites, its better to create the connection in another file krn file utama nya
+# lama kelamaan akan numpuk dengan function calls and code yg lain dan bakal susah untuk dimanage.
 
+# import MySQLdb > jadi nya dibuat disini deh krn gw gtau cr pake connection ny dr file lain gimana jd ribet
+# from __init__ import mysqlconnection
 
 
 main = Blueprint('main', __name__)
+# mysql = MySQL(main) > kalo pake flask_mysqldb import MySQL
 
 @main.route('/')
 def index():
@@ -61,15 +70,47 @@ def profile():
 		endy = request.form['save_endy']
 		tool = request.form['save_tool']
 		color = request.form['save_color']
+		
+		conn = MySQLdb.connect(host="localhost",user = "root",password = "root",db = "flask")
+		c = conn.cursor()
+
+		# try:
+		# 	c, conn = mysqlconnection()
+		# 	return("Okay")
+		# except Exception as e:
+		# 	return(str(e))
+
+		# c.execute("SELECT * FROM flask")
 
 		dbdata = (imagename,label,startx,starty,endx,endy,tool,color)
 		
-		con = sqlite3.connect("Annotations.db")
-		cur=con.cursor()
-		cur.execute("CREATE TABLE IF NOT EXISTS Coordinates(Image text,Label text, StartX text, StartY text, EndX text, EndY text, Tool text, Color text)")
-		cur.execute("INSERT INTO Coordinates VALUES(?, ?, ?, ?, ?, ?, ?, ?)", dbdata)
-		con.commit()
-		con.close()
+		c.execute("CREATE TABLE IF NOT EXISTS Coordinates(Image text,Label text, StartX text, StartY text, EndX text, EndY text, Tool text, Color text)")
+		# c.execute('INSERT INTO Coordinates VALUES(?, ?, ?, ?, ?, ?, ?, ?)', dbdata) > pake ini error mysql
+		c.execute('INSERT INTO Coordinates(Image, Label, StartX, StartY, EndX, EndY, Tool, Color) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)', dbdata)
+
+		conn.commit()
+		conn.close()
+		return 'success~'
+
+# POST Method DB SQLITE SEBELUM MIGRATE KE MYSQL
+	# if request.method == 'POST':
+	# 	imagename = request.form['save_imagename']
+	# 	label = request.form['save_label']
+	# 	startx = request.form['save_startx']
+	# 	starty = request.form['save_starty']
+	# 	endx = request.form['save_endx']
+	# 	endy = request.form['save_endy']
+	# 	tool = request.form['save_tool']
+	# 	color = request.form['save_color']
+
+	# 	dbdata = (imagename,label,startx,starty,endx,endy,tool,color)
+		
+	# 	con = sqlite3.connect("Annotations.db")
+	# 	cur=con.cursor()
+	# 	cur.execute("CREATE TABLE IF NOT EXISTS Coordinates(Image text,Label text, StartX text, StartY text, EndX text, EndY text, Tool text, Color text)")
+	# 	cur.execute("INSERT INTO Coordinates VALUES(?, ?, ?, ?, ?, ?, ?, ?)", dbdata)
+	# 	con.commit()
+	# 	con.close()
 		
 	
 # POST Method using SQLITE DB with 3 data values
@@ -88,107 +129,43 @@ def profile():
 	# 	con.close()
 		
 	
-@main.route('/save', methods=['GET', 'POST'])
-def save():
-	con = sqlite3.connect("Drawings.db")
-	cur = con.cursor()
-	cur.execute("SELECT name, data, canvas_image from files")
-	files = cur.fetchall()
-	con.close()
-	return render_template("save.html", files = files )
-	
-@main.route('/search', methods=['GET', 'POST'])
-def search():
-	if request.method == 'GET':
-		return render_template("search.html")
-	if request.method == 'POST':
-		filename = request.form['fname']
-		con = sqlite3.connect("Drawings.db")
-		cur = con.cursor()
-		cur.execute("select name, data, canvas_image from files")
-		files = cur.fetchall()
-		con.close()
-		return render_template("search.html", files=files, filename=filename)
-
-# @main.route('/tagger')
-# def tagger():
-#     if (main.config["HEAD"] == len(app.config["FILES"])):
-#         return redirect(url_for('bye'))
-#     directory = app.config['IMAGES']
-#     image = app.config["FILES"][app.config["HEAD"]]
-#     labels = app.config["LABELS"]
-#     not_end = not(app.config["HEAD"] == len(app.config["FILES"]) - 1)
-#     print(not_end)
-#     return render_template('tagger.html', not_end=not_end, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
-
-# @main.route('/next')
-# def next():
-#     image = app.config["FILES"][app.config["HEAD"]]
-#     app.config["HEAD"] = app.config["HEAD"] + 1
-#     with open(app.config["OUT"],'a') as f:
-#         for label in app.config["LABELS"]:
-#             f.write(image + "," +
-#             label["id"] + "," +
-#             label["name"] + "," +
-#             str(round(float(label["xMin"]))) + "," +
-#             str(round(float(label["xMax"]))) + "," +
-#             str(round(float(label["yMin"]))) + "," +
-#             str(round(float(label["yMax"]))) + "\n")
-#     app.config["LABELS"] = []
-#     return redirect(url_for('tagger'))
-
-# @main.route("/bye")
-# def bye():
-#     return send_file("taf.gif", mimetype='image/gif')
-
-# @main.route('/add/<id>')
-# def add(id):
-#     xMin = request.args.get("xMin")
-#     xMax = request.args.get("xMax")
-#     yMin = request.args.get("yMin")
-#     yMax = request.args.get("yMax")
-#     app.config["LABELS"].append({"id":id, "name":"", "xMin":xMin, "xMax":xMax, "yMin":yMin, "yMax":yMax})
-#     return redirect(url_for('tagger'))
-
-# @main.route('/remove/<id>')
-# def remove(id):
-#     index = int(id) - 1
-#     del app.config["LABELS"][index]
-#     for label in app.config["LABELS"][index:]:
-#         label["id"] = str(int(label["id"]) - 1)
-#     return redirect(url_for('tagger'))
-
-# @main.route('/label/<id>')
-# def label(id):
-#     name = request.args.get("name")
-#     app.config["LABELS"][int(id) - 1]["name"] = name
-#     return redirect(url_for('tagger'))
-
-# @app.route('/prev')
-# def prev():
-#     app.config["HEAD"] = app.config["HEAD"] - 1
-#     return redirect(url_for('tagger'))
-
-# @main.route('/image/<f>')
-# def images(f):
-#     images = app.config['IMAGES']
-#     return send_file(images + f)
-
-# @main.route('/profile')
-# @login_required
-# def profile():
-#     return render_template('marker.html', name=current_user.name)
-
 # @main.route('/save', methods=['GET', 'POST'])
 # def save():
-# 	conn = psycopg2.connect(database="tutorial", user = "postgres", password = "Password1234", host = "localhost" , port = "5432")
-# 	cur = conn.cursor()
-# 	cur.execute("SELECT id, name, data, canvas_image from files")
-# 	files = cur.fetchall()
-# 	conn.close()
-# 	return render_template("save.html", files = files )
+	# con = sqlite3.connect("Drawings.db")
+	# cur = con.cursor()
+	# cur.execute("SELECT name, data, canvas_image from files")
+	# files = cur.fetchall()
+	# con.close()
+	# return render_template("save.html", files = files )
+	
+# @main.route('/search', methods=['GET', 'POST'])
+# def search():
+	# if request.method == 'GET':
+	# 	return render_template("search.html")
+	# if request.method == 'POST':
+	# 	filename = request.form['fname']
+	# 	con = sqlite3.connect("Drawings.db")
+	# 	cur = con.cursor()
+	# 	cur.execute("select name, data, canvas_image from files")
+	# 	files = cur.fetchall()
+	# 	con.close()
+	# 	return render_template("search.html", files=files, filename=filename)
 
-	# POST Method using SQLITE DB with 2 data values
+# @main.route('/profile')
+	# @login_required
+	# def profile():
+	#     return render_template('marker.html', name=current_user.name)
+
+# @main.route('/save', methods=['GET', 'POST'])
+	# def save():
+	# 	conn = psycopg2.connect(database="tutorial", user = "postgres", password = "Password1234", host = "localhost" , port = "5432")
+	# 	cur = conn.cursor()
+	# 	cur.execute("SELECT id, name, data, canvas_image from files")
+	# 	files = cur.fetchall()
+	# 	conn.close()
+	# 	return render_template("save.html", files = files )
+
+# POST Method using SQLITE DB with 2 data values
 	# if request.method == 'POST':
 	# 	filename=request.form['save_fname']
 	# 	data=request.form['save_cdata']
@@ -204,7 +181,7 @@ def search():
 	# 	resp = Response("saved")
 	# 	return resp
 
-	# POST Method Using POSTGRESQL with 3 values
+# POST Method Using POSTGRESQL with 3 values
 	# if request.method == 'POST':
 	#     filename = request.form['save_fname']
 	#     data = request.form['save_cdata']
