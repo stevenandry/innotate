3209b945940ea4783eafd80f46d845509192fe34
Steven <stevenandryy@gmail.com>
2020-05-17 23:57:54 +0700

    Result page now working. added bunch of stuff

--- static/script-result.js
+++ static/script-result.js
@@ -1,11 +1,17 @@
 var imagearray = [], imagenamearray = [], imagelabelarray = [], colorarray = []
     , annotateindexarray = [], filteredimagearray = [], toolarray = [], annotatorarray = []
-    , startxarray = [], startyarray = [], endxarray = [], endyarray = [], annotationidarray = [];
+    , startxarray = [], startyarray = [], endxarray = [], endyarray = [], annotationidarray = [], yourimageunique = []
 var yourresultarray = [], yourannotationid = [], yourannotationidunique = [];
 var yourannotationimgid = [], yourantnimgidunique = [], imgrealwidth, imgrealheight;
 var yourresult = document.getElementById("yourresult");
 var otherresult = document.getElementById("otherresult");
 var username = document.getElementById("username").textContent;
+var otherusernameholder = "";
+
+$(window).on("load", function () {
+    $(".loader").fadeOut("fast");
+    setTimeout(function(){newload();},200);
+});
 
 $(document).ready(function () {
     $('.imageclass').each(function (index, element) {
@@ -55,27 +61,61 @@ $(document).ready(function () {
         annotatorarray.push($(element).text());
         //alert($(element).text());
     });
-    $('.annotateindexclass').each(function (index, element) {
-        annotateindexarray.push($(element).text());
-        //alert($(element).text());
-    });
-    $('.annotationidclass').each(function (index, element) {
-        annotationidarray.push($(element).text());
-        // alert($(element).text());
-    });
+
     $('[data-toggle="popover"]').popover();
+
+    $("#imagesearchbar").keyup(function() {
+        var val = $.trim(this.value);
+        if (val === ""){
+            $('img').show();
+            $('svg').show();
+            $(".loader").fadeOut("fast");
+        }
+        else {
+            $('img').hide();
+            $('svg').hide();
+            $("img[alt*=" + val + "]").show();
+            $("svg[id*=" + val + "]").show();
+        }
+    });
+
     loadimage();
     createyourresult();
-    createyourannotationid();
-    createyourimgid();
-    // var canvas = document.getElementById("paint");
-    
-    // var ctx = canvas.getContext("2d");
-    
-    // var test = document.getElementById("images10.jpg");
-    // ctx.drawImage(test, 0, 0, canvas.width, canvas.height);
+    createotherresult();
+    listotherannotator();
+    // createyourannotationid();
+    // createyourimgid();
+
     
 });
+
+function listotherannotator(){
+    var temparray = [];
+    for (var i = 0; i < annotatorarray.length; i++) {
+        if (annotatorarray[i] != username) {
+            temparray.push({
+                annotator: annotatorarray[i]
+            });
+        }
+    }
+    otherannotatorunique = _.uniqBy(temparray, function (e) {
+        return e.annotator;
+    });
+    
+    for( var i =0;i<otherannotatorunique.length;i++){
+        var annotator = "";
+        annotator += "<option>"+otherannotatorunique[i].annotator+"</option>";
+        document.getElementById("selectusername").innerHTML += annotator;
+    }
+}
+
+function selectusername(){
+    var otherusername= document.getElementById("selectusername").value;
+    if(otherusername != otherusernameholder){
+        $('#otherresultlist div').html('');
+        createotherresult(otherusername);
+    }
+}
 
 function loadimage() {
     var html = "";
@@ -91,7 +131,7 @@ function createyourresult() {
             yourresultarray.push({
                 imagename: imagenamearray[i], imagelabel: imagelabelarray[i], startx: startxarray[i], starty: startyarray[i]
                 , endx: endxarray[i], endy: endyarray[i], tool: toolarray[i], color: colorarray[i], annotator: annotatorarray[i]
-                , annotationindex: annotateindexarray[i], annotationid: annotationidarray[i]
+                //, annotationindex: annotateindexarray[i], annotationid: annotationidarray[i]
             });
             // alert(yourresultarray[0].imagename+yourresultarray[0].imagelabel+yourresultarray[0].startx
             //     +yourresultarray[0].starty+yourresultarray[0].endx+yourresultarray[0].endy
@@ -99,6 +139,62 @@ function createyourresult() {
             //     +yourresultarray[0].annotationindex+yourresultarray[0].annotationid);
         }
     }
+    yourimageunique = _.uniqBy(yourresultarray, function (e) {
+        return e.imagename;
+    });
+    // for(var i =0;i<yourimageunique.length;i++){
+    //     alert(yourimageunique[i].imagename);
+    // }
+}
+
+function createotherresult(otherusername){
+    var otherresultarray =[], otherimageunique=[]; 
+    for (var i = 0; i < annotatorarray.length; i++) {
+        if (annotatorarray[i] == otherusername) {
+            otherresultarray.push({
+                imagename: imagenamearray[i], imagelabel: imagelabelarray[i], startx: startxarray[i], starty: startyarray[i]
+                , endx: endxarray[i], endy: endyarray[i], tool: toolarray[i], color: colorarray[i], annotator: annotatorarray[i]
+            });
+        }
+    }
+    otherimageunique = _.uniqBy(otherresultarray, function (e) {
+        return e.imagename;
+    });
+    for (var i = 0; i < otherimageunique.length; i++) {
+        //alert(otherimageunique[i].imagename);
+        var images = '<div style="position:relative;float:left">'
+				+'<img src="static/images/'+otherimageunique[i].imagename+'" class="mb-3 mr-3 rounded" width=300 height=300>'
+                // + '<img class="loader" src="static/loadinggif.gif" width=50 height=50 style="z-index:2 position: absolute; left: 50%; top: 50%; margin: -<25> 0 0 -<25>">'
+				+'<svg class="hvr-border-fade" id="svgovercanvas" style="position:absolute;left: 0px; top:0px;z-index: 2;cursor:pointer" width=300 height=300> ';
+        var image = document.getElementById(otherimageunique[i].imagename);
+        imgrealwidth = image.width;
+        imgrealheight = image.height;
+       //alert(imgrealheight + " " + imgrealwidth);
+           
+        for (var y = 0; y < otherresultarray.length; y++) {
+            if(otherresultarray[y].imagename == otherimageunique[i].imagename){
+                if (otherresultarray[y].tool == 'Dot Tool') {
+                    var convertstartx = otherresultarray[y].startx * 300 / imgrealwidth;
+                    var convertstarty = otherresultarray[y].starty * 300 / imgrealheight;
+                    images += '<circle cx="'+convertstartx+'" cy="'+convertstarty+'" r="3" fill="'+otherresultarray[y].color+'" />';
+                } else {
+                    var convertstartx = otherresultarray[y].startx * 300 / imgrealwidth;
+                    var convertstarty = otherresultarray[y].starty * 300 / imgrealheight;
+                    var convertendx = otherresultarray[y].endx * 300 / imgrealwidth;
+                    var convertendy = otherresultarray[y].endy * 300 / imgrealheight;
+                    var width = convertendx - convertstartx;
+                    var height = convertendy - convertstarty;
+                    var labelY = convertstarty - 3;
+                    images += '<rect x="'+convertstartx+'" y="'+convertstarty+'" width="'+width+'" height="'+height+'" fill="none" stroke="'+otherresultarray[y].color+'" stroke-width="3"/>'
+                        + '<text x="' + convertstartx + '" y="' + labelY + '" fill="' + otherresultarray[y].color + '">' + otherresultarray[y].imagelabel + '</text>';
+                }
+            }
+        }
+        images += '</svg></div>';
+        document.getElementById("otherresultlist").innerHTML += images;
+        // alert("success");
+    }
+    otherusernameholder = otherusername;
 }
 
 function createyourannotationid() {
@@ -132,54 +228,23 @@ function createyourimgid() {
 
 }
 
-function test(){
-     for (var i = 0; i < yourantnimgidunique.length; i++) {
-        alert(yourantnimgidunique[i].annotationid);
-        var images = '<canvas class="mr-3 mb-3 hvr-grow" id="'+yourantnimgidunique[i].annotationid +'" width="300" height="300"></canvas>';
-        document.getElementById("yourresultlist").innerHTML += images;
-        var canvas = document.getElementById(yourantnimgidunique[i].annotationid);
-        var ctx = canvas.getContext("2d");
-        var image = document.getElementById(yourantnimgidunique[i].imagename);
+function newload(){
+    for (var i = 0; i < yourimageunique.length; i++) {
+        var images = '<div style="position:relative;float:left" onclick="changeimage(\''+yourimageunique[i].imagename+'\')">'
+				+'<img src="static/images/'+yourimageunique[i].imagename+'" alt="'+yourimageunique[i].imagename+'" class="mb-3 mr-3 rounded" width=300 height=300>'
+				+'<svg class="hvr-border-fade" id="'+yourimageunique[i].imagename+'" style="position:absolute;left: 0px; top:0px;z-index: 2;cursor:pointer" width=300 height=300> '
+                +'<image id="loading-'+yourimageunique[i].imagename+'" style="display:none" href="static/loadinggif.gif" x="120" y="130" height="50px" width="50px"/>';
+        var image = document.getElementById(yourimageunique[i].imagename);
         imgrealwidth = image.width;
         imgrealheight = image.height;
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
+        //alert(imgrealheight + " " + imgrealwidth);
+           
         for (var y = 0; y < yourresultarray.length; y++) {
-            if (yourantnimgidunique[i].annotationid == yourresultarray[y].annotationid) {
+            if(yourresultarray[y].imagename == yourimageunique[i].imagename){
                 if (yourresultarray[y].tool == 'Dot Tool') {
                     var convertstartx = yourresultarray[y].startx * 300 / imgrealwidth;
                     var convertstarty = yourresultarray[y].starty * 300 / imgrealheight;
-                    ctx.beginPath();
-                    ctx.arc(convertstartx, convertstarty, 4, 0, Math.PI * 2, true);
-                    ctx.fill();
-                } else {
-                    // var convertstartx = yourresultarray[y].startx * 300 / imgrealwidth;
-                    // var convertstarty = yourresultarray[y].starty * 300 / imgrealheight;
-                    // var convertendx = yourresultarray[y].endx * 300 / imgrealwidth;
-                    // var convertendy = yourresultarray[y].endy * 300 / imgrealheight;
-                    // var width = convertendx - convertstartx;
-                    // var height = convertendy - convertstarty;
-                    // images += '<rect x="'+convertstartx+'" y="'+convertstarty+'" width="'+width+'" height="'+height+'" fill="none" stroke="red" stroke-width="4"/>';
-                }
-            }
-        }
-        // alert("success");
-    }
-}
-
-function test2() {
-    for (var i = 0; i < yourantnimgidunique.length; i++) {
-        var images = '<div style="position:relative"><img src="static/images/'+yourantnimgidunique[i].imagename+'" class="mb-3 mr-3 hvr-grow" width=300 height=300>'
-				+'<svg class="hvr-fade" id="svgovercanvas" style="position:absolute;left: 0px; top:0px;z-index: 2;cursor:pointer"width=300 height=300> ';
-        var getimage = document.getElementById(yourannotationimgid[i].imagename);
-        imgrealwidth = getimage.width;
-        imgrealheight = getimage.height;
-        for (var y = 0; y < yourresultarray.length; y++) {
-            if (yourantnimgidunique[i].annotationid == yourresultarray[y].annotationid) {
-                if (yourresultarray[y].tool == 'Dot Tool') {
-                    var convertstartx = yourresultarray[y].startx * 300 / imgrealwidth;
-                    var convertstarty = yourresultarray[y].starty * 300 / imgrealheight;
-                    images += '<circle cx="'+convertstartx+'" cy="'+convertstarty+'" r="4" fill="red" />';
+                    images += '<circle cx="'+convertstartx+'" cy="'+convertstarty+'" r="3" fill="'+yourresultarray[y].color+'" />';
                 } else {
                     var convertstartx = yourresultarray[y].startx * 300 / imgrealwidth;
                     var convertstarty = yourresultarray[y].starty * 300 / imgrealheight;
@@ -187,7 +252,9 @@ function test2() {
                     var convertendy = yourresultarray[y].endy * 300 / imgrealheight;
                     var width = convertendx - convertstartx;
                     var height = convertendy - convertstarty;
-                    images += '<rect x="'+convertstartx+'" y="'+convertstarty+'" width="'+width+'" height="'+height+'" fill="none" stroke="red" stroke-width="4"/>';
+                    var labelY = convertstarty - 3;
+                    images += '<rect x="'+convertstartx+'" y="'+convertstarty+'" width="'+width+'" height="'+height+'" fill="none" stroke="'+yourresultarray[y].color+'" stroke-width="3"/>'
+                        + '<text x="' + convertstartx + '" y="' + labelY + '" fill="' + yourresultarray[y].color + '">' + yourresultarray[y].imagelabel + '</text>';
                 }
             }
         }
@@ -195,91 +262,30 @@ function test2() {
         document.getElementById("yourresultlist").innerHTML += images;
         // alert("success");
     }
+
 }
 
-// function loadresult() {
-//     // var images = '<svg width="300" height="300" style="cursor:pointer" class="mr-3 mb-3 hvr-grow">' 
-//     // 			+'<image xlink:href="static/images/images (2).jpeg" width="300" height="300" x="0" y="0" />'
-//     // 		+'</svg>';
-//     //bikin list baru dari yourresultarray untuk khusus annotationid aja, nanti dibikin unique
-//     //bikin list baru dari yourresultarray jg untuk annotationid dan image, utk di compare
-//     for (var i = 0; i < annotationidarray.length; i++) {
-//         var images = '<svg width="300" height="300" style="cursor:pointer" class="mr-3 mb-3 hvr-grow">';
-//         for(var z=0;z< imageannotationid.length; z++){
-//             if(annotationidarray[i] == imageannotationid[z].annotationid){
-//                 images+='<image xlink:href="static/images/images (2).jpeg" width="300" height="300" x="0" y="0" />';
-//             }
-//         }
-
-//         for (var y = 0; y < yourresultarray.length; i++) {
-//             if (annotationidarray[i] == yourresultarray[y].annotationid) {
-//                 if (yourresultarray[y].tool == 'Dot Tool') {
-//                     images += '<circle id="testcircle" cx="50" cy="50" r="40" fill="yellow" />';
-
-//                 } else {
-//                     images += '<rect>'
-//                 }
-//             }
-//         }
-//         images += '</svg>';
-//         document.getElementById("yourresultlist").innerHTML += images;
-//     }
-// }
-
-// function removeDuplicates(myArr, prop) {
-//     return myArr.filter((obj, pos, arr) => {
-//         return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
-//     });
-// }
-// for(var i = 0;i<yourannotationidunique.length;i++){
-//     for(var y =0;y<yourresultarray.length;y++){
-//         if(yourresultarray[y].annotationid == yourannotationidunique[i]){
-//             for( var z=0;z<yourannotationimgid.length;z++){
-//                 var check = yourannotationimgid[z].annotationid.includes(yourannotationidunique[i]);
-//                 if(check == false){
-//                     yourannotationimgid.push({ imagename: yourresultarray[i].imagename, annotationid: yourresultarray[i].annotationid });
-//                     alert("success");
-//                 }
-//             }
-//         }
-//     }
-// }
-
-function loadyourresult(){
-    for (var i = 0; i < yourantnimgidunique.length; i++) {
-        var images = '<svg width="300" height="300" style="cursor:pointer" class="mr-3 mb-3 hvr-grow">';
-        images += '<image xlink:href="static/images/'+ yourantnimgidunique[i].imagename +'" width="300" height="300" x="0" y="0" />';
-        // alert("1"); //passed
-        for(var z=0;z<imagearray.length;z++){
-            if(yourantnimgidunique[i].imagename == imagearray[z]){
-                var image = document.getElementById(imagearray[z]);
-                imgrealwidth = image.width;
-                imgrealheight = image.height;
-                // alert(imgrealheight + " " + imgrealwidth);
-            }
+function changeimage(imagedata){
+    //alert("running");
+    document.getElementById("loading-"+imagedata+"").style.display = "inline";
+    for(var i=0;i<imagearray.length;i++){
+        if(imagearray[i] == imagedata){
+            var cursor = i;
         }
-        for (var y = 0; y < yourresultarray.length; y++) {
-            if (yourantnimgidunique[i].annotationid == yourresultarray[y].annotationid) {
-                if (yourresultarray[y].tool == 'Dot Tool') {
-                    var convertstartx = yourresultarray[y].startx * 300 / imgrealwidth;
-                    var convertstarty = yourresultarray[y].starty * 300 / imgrealheight;
-                    images += '<circle cx="'+convertstartx+'" cy="'+convertstarty+'" r="4" fill="red" />';
-                } else {
-                    var convertstartx = yourresultarray[y].startx * 300 / imgrealwidth;
-                    var convertstarty = yourresultarray[y].starty * 300 / imgrealheight;
-                    var convertendx = yourresultarray[y].endx * 300 / imgrealwidth;
-                    var convertendy = yourresultarray[y].endy * 300 / imgrealheight;
-                    var width = convertendx - convertstartx;
-                    var height = convertendy - convertstarty;
-                    images += '<rect x="'+convertstartx+'" y="'+convertstarty+'" width="'+width+'" height="'+height+'" fill="none" stroke="red" stroke-width="4"/>';
-                }
-            }
-        }
-        images += '</svg>';
-        document.getElementById("yourresultlist").innerHTML += images;
-        // alert("success");
     }
-
+    var updatecursor = ++cursor;
+    //$.post("/updatecursor", { update_cursor: updatecursor });
+    //setTimeout( function (){window.location.href = "/resize";},750);
+    var xhr = new XMLHttpRequest();
+    var url = "/updatecursor";
+    xhr.open("POST", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+                window.location.href = "/resize";
+            }
+        };
+    //alert(updatecursor);
+    xhr.send(updatecursor);
 }
 
 function alerttest() {
